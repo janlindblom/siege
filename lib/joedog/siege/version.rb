da8177cf58de022f4225e6e8a3adb219b515cb76
Jan Lindblom <jan.lindblom@mittmedia.se>
2014-01-20 15:32:39 +0100

    Started porting.

---
+++ lib/joedog/siege/version.rb
@@ -0,0 +1,10 @@
+module Joedog
+  module Siege
+    VERSION       = "3.0.6"
+    PROGRAM_NAME  = "siege"
+    AUTHOR_NAME   = "Jeffrey Fulmer, et al."
+    EMAIL_ADDRESS = "jeff@joedog.org"
+    YEARS         = "1999-2014"
+    COPYRIGHT     = "Copyright (C) 2014 by Jeffrey Fulmer, et al.\nThis is free software; see the source for copying conditions.\nThere is NO warranty; not even for MERCHANTABILITY or FITNESS\nFOR A PARTICULAR PURPOSE.\n"
+  end
+end
